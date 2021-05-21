bc6553e91af18db4a7e739c3ef132f1f95b2ba7a
modbots <77435462+modbots@users.noreply.github.com>
2021-05-22 00:21:42 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,36 @@
 import os
+BOT_TOKEN = "11406408953:AAGh-8WK3Sq6lnHWCjbkMDXBYQguTMRewiI"
+RESULTS_COUNT = 4  # NOTE Number of results to show, 4 is better
+SUDO_CHATS_ID = [-1001450359206]
 
+
+DRIVE_NAME = [
+    "Root",  # folder 1 name
+    "Cartoon",  # folder 2 name
+    "Course",  # folder 3 name
+    "Movies",  # ....
+    "Series",  # ......
+    "Others"  # and soo onnnn folder n names
+]
+
+DRIVE_ID = [
+    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8",  # folder 1 id
+    "12wNJTjNnR-CNBOTnLHqe-1vqFvCRLecn",  # folder 2 id
+    "11nZcObsJJHojHYg43dBS0_eVvJrSD7Nf",  # and so onn... folder n id
+    "10_hTMK8HE8k144wOTth_3x1hC2kZL-LR",
+    "1-oTctBpyFcydDNiptLL09Enwte0dClCq",
+    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8"
+]
+
+INDEX_URL = [
+    "https://dl.null.tech/0:",  # folder 1 index link
+    "https://dl.null.tech/0:/Cartoon",  # folder 2 index link
+    "https://dl.null.tech/0:/Course",  # and soo on folder n link
+    "https://dl.null.tech/0:/MOVIES",
+    "https://dl.null.tech/0:/Series",
+    "https://dl.null.tech/0:/Roms"
+]
+© 2021 GitHub, Inc.
 class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = ""
