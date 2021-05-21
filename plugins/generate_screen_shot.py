80d4170045e06cdb5f86317610d9547eaf5bf639
modbots <77435462+modbots@users.noreply.github.com>
2021-05-21 15:53:31 +0630

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -27,7 +27,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from helper_funcs.display_progress import progress_for_pyrogram
-
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["generatescss"]))
 async def generate_screen_shot(bot, update):
