091862904ae55430641257d09ac22d7fb56e6dea
modbots <77435462+modbots@users.noreply.github.com>
2021-05-24 10:33:46 +0630

    Update maxdl.py

--- plugins/maxdl.py
+++ plugins/maxdl.py
@@ -57,13 +57,8 @@ async def maxdl_dl(bot, update):
                 message_id=a.message_id
             )
         for url in open('urls.txt'):
-    # Split on the rightmost / and take everything on the right side of that
            name = url.rsplit('/', 1)[-1]
-
-    # Combine the name and the downloads directory to get the local filename
            filename = os.path.join(DOWNLOADS_DIR, name)
-
-    # Download the file if it does not exist
            if not os.path.isfile(filename):
               urllib.urlretrieve(url, filename)
     else:
